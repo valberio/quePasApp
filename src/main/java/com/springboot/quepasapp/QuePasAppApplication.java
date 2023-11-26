2436658afcc305bd961a2131c63a9c319556e3d0
Valen <valberio@frba.utn.edu.ar>
2023-11-26 16:01:34 -0300

    Initial commit

---
+++ src/main/java/com/springboot/quepasapp/QuePasAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.quepasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuePasAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuePasAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/quepasapp/QuePasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.quepasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuePasAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
